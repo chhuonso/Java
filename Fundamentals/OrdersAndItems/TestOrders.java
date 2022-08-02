fc2e6b22fe4fe9058c97a4470f88953f631076e9
chhuonso <103080113+chhuonso@users.noreply.github.com>
2022-08-02 13:54:58 -0700

    Order and Items

---
+++ Fundamentals/OrdersAndItems/TestOrders.java
@@ -0,0 +1,93 @@
+import java.util.ArrayList;
+public class TestOrders {
+    public static void main(String[] args) {
+        // System.out.println("hello");
+        // Menu items
+        Item item1 = new Item();
+        item1.name = "mocha";
+        item1.price = 5.0;
+        
+        Item item2 = new Item();
+        item2.name = "latte";
+        item2.price = 6.5;
+        
+        Item item3 = new Item();
+        item3.name = "drip coffee";
+        item3.price = 7.5;
+        
+        Item item4 = new Item();
+        item4.name = "cappuccino";
+        item4.price = 8.5;
+
+
+
+        // Order variables -- order1, order2 etc.
+        Order order1 = new Order();
+        System.out.println(order1.total);
+        order1.name = "Cindhurt";
+
+
+        Order order2 = new Order();
+        order2.name = "Jimmy";
+
+
+        Order order3 = new Order();
+        order3.name = "Naoh";
+
+
+        Order order4 = new Order();
+        order4.name = "Sam";
+
+
+
+        // Application Simulations
+        // Use this example code to test various orders' updates
+
+        // Add item1 to order2's item list and increment the order's total.
+        order2.items.add(item1);
+        order2.total += item1.price;
+
+        // order3 ordered a cappucino. Add the cappuccino to their order list and to their tab.
+        order3.items.add(item4);
+        order3.total += item4.price;
+
+        // order4 added a latte. Update accordingly.
+        order4.items.add(item2);
+        order4.total += item2.price;
+
+        // Cindhuri’s order is now ready. Update her status.
+        order1.ready = true;
+
+        // Sam ordered more drinks - 2 lattes. Update their order as well.
+        order4.items.add(item2);
+        order4.items.add(item2);
+        order4.total += item2.price * 2;
+
+        // Jimmy’s order is now ready. Update his status.
+        order2.ready = true;
+
+
+System.out.println("********* ORDER NUMBER 1 ***********");
+        System.out.printf("Name: %s\n", order1.name);
+        System.out.printf("Total: %s\n", order1.total);
+        System.out.printf("Ready: %s\n", order1.ready);
+
+System.out.println("********* ORDER NUMBER 2 ***********");
+        System.out.printf("Name: %s\n", order2.name);
+        System.out.printf("Total: %s\n", order2.total);
+        System.out.printf("Ready: %s\n", order2.ready);
+
+System.out.println("********* ORDER NUMBER 3 ***********");
+        System.out.printf("Name: %s\n", order3.name);
+        System.out.printf("Total: %s\n", order3.total);
+        System.out.printf("Ready: %s\n", order3.ready);
+
+System.out.println("********* ORDER NUMBER 4 ***********");
+        System.out.printf("Name: %s\n", order4.name);
+        System.out.printf("Total: %s\n", order4.total);
+        System.out.printf("Ready: %s\n", order4.ready);
+
+
+    }
+}
+
