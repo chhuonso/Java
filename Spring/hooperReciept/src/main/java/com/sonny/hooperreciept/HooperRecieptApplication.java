7f15320a46d35d62ecf3eef7073d21b868027af4
chhuonso <103080113+chhuonso@users.noreply.github.com>
2022-08-19 21:34:13 -0700

    hooper java

---
+++ Spring/hooperReciept/src/main/java/com/sonny/hooperreciept/HooperRecieptApplication.java
@@ -0,0 +1,13 @@
+package com.sonny.hooperreciept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HooperRecieptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HooperRecieptApplication.class, args);
+	}
+
+}
