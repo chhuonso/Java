c4781f59240a144c8410423d55b80350a84aed72
chhuonso <103080113+chhuonso@users.noreply.github.com>
2022-08-19 21:37:24 -0700

    test ftyloops

---
+++ Spring/fruityLoops/src/test/java/com/sonny/fruityloops/FruityLoopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonny.fruityloops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruityLoopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
