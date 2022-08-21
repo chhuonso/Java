201bba873527fdbea14c14bbcdafd2f898ea2314
chhuonso <103080113+chhuonso@users.noreply.github.com>
2022-08-21 16:54:49 -0700

    saveTravel

---
+++ Spring/wk2dy3/src/test/java/com/sonny/wk2dy3/Wk2dy3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonny.wk2dy3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wk2dy3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
