f473934d02d345c4ed7c6d68a9c94a98c8bf6591
chhuonso <103080113+chhuonso@users.noreply.github.com>
2022-08-21 16:58:17 -0700

    all files for folder

---
+++ Spring/wk2dy3/src/main/java/com/sonny/wk2dy3/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sonny.wk2dy3;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Wk2dy3Application.class);
+	}
+
+}
---
+++ Spring/wk2dy3/src/main/java/com/sonny/wk2dy3/Wk2dy3Application.java
@@ -0,0 +1,13 @@
+package com.sonny.wk2dy3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wk2dy3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wk2dy3Application.class, args);
+	}
+
+}
