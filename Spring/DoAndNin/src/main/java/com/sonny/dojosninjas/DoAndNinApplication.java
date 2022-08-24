bc18ae9c86ec4562784b7a42e4f1eb0aca651a05
chhuonso <103080113+chhuonso@users.noreply.github.com>
2022-08-23 19:29:08 -0700

    pom xml and properties

---
+++ Spring/DoAndNin/src/main/java/com/sonny/dojosninjas/DoAndNinApplication.java
@@ -0,0 +1,13 @@
+package com.sonny.dojosninjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoAndNinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoAndNinApplication.class, args);
+	}
+
+}
